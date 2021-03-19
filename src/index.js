73ddf8779538e59228ba1a5cec2a6435ca9138d4
Nikita Yutanov <nikitayutanov@gmail.com>
2021-03-20 04:26:25 +0500

    Add node-sass and gh-pages dependencies, absolute imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './index.css';
+import 'assets/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from 'App';
 
 ReactDOM.render(
   <React.StrictMode>
