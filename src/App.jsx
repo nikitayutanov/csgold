f928a85a2136559aa5ca5da6c1d4e84ce0d367c3
Nikita Yutanov <nikitayutanov@gmail.com>
2021-04-20 09:36:02 +0500

    Add Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { pageview } from 'react-ga';
 import Header from 'components/header/Header';
 import Footer from 'components/footer/Footer';
 import Main from 'components/main/Main';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const { pathname } = location;
+    pageview(pathname);
+  }, [location]);
+
   return (
     <>
       <Header />
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import 'assets/style.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import App from 'App';
+
+ReactGA.initialize('UA-170474725-1');
 
 ReactDOM.render(
   <React.StrictMode>
